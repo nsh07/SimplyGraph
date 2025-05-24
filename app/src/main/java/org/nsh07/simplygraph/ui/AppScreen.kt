31de1dde0943d419f9f51de4b235aecf0c34e50b
Nishant Mishra <66912344+nsh07@users.noreply.github.com>
2025-05-24 08:34:07 +0530

    Some more UI and exception improvements
    Functions with division by 0 and other indefinite results at some points in the domain can now render

--- app/src/main/java/org/nsh07/simplygraph/ui/AppScreen.kt
+++ app/src/main/java/org/nsh07/simplygraph/ui/AppScreen.kt
@@ -1,6 +1,8 @@
 package org.nsh07.simplygraph.ui
 
+import android.R.attr.x
 import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -8,13 +10,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -22,7 +21,9 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.PointMode
@@ -37,38 +38,16 @@ import org.nsh07.simplygraph.ui.theme.SimplyGraphTheme
 fun AppScreen(modifier: Modifier = Modifier) {
     val xWidth = 10 // Number of integral x-coordinates visible on screen
     val colorScheme = colorScheme
-    var function by remember { mutableStateOf("x") }
+    var function by remember { mutableStateOf("") }
     var canvasSize by remember { mutableStateOf(Size(0f, 0f)) }
     var points = emptyList<Offset>()
     var invalidations by remember { mutableIntStateOf(0) }
-    var redrawCount by remember { mutableIntStateOf(0) }
 
-    LaunchedEffect(redrawCount) {
+    LaunchedEffect(function) {
         // Evaluate expression at various points along the x-axis and find y-values accordingly
         if (function.isNotEmpty()) {
             try {
-                val exp = ExpressionBuilder(function)
-                    .variable("x")
-                    .build()
-
-                val newPoints = mutableListOf<Offset>()
-                val widthInt = canvasSize.width.toInt()
-
-                for (i in 0..widthInt) {
-                    // Calculate the value of x and y for the given i-pixel-offset
-                    // x can be simply calculated by subtracting half of canvas width (to shift origin to half of the screen) and then scaling according to xWidth
-                    val x = ((i.toDouble() - canvasSize.width / 2) / canvasSize.width) * xWidth
-
-                    // Calculating y is a bit more complex, we find the value of f(x) for x as calculated above,
-                    // then multiply it by the below factor to scale it to keep the x and y-axis scale uniform in the graph
-                    // We then subtract this value from half of the canvas width (to shift origin to half the screen)
-                    // And voila, we get a graph with the origin at the center of the screen.
-                    val y =
-                        (-exp.setVariable("x", x).evaluate() * (canvasSize.height / (xWidth * (canvasSize.height / canvasSize.width)))) + canvasSize.height / 2
-                    newPoints.add(Offset(i.toFloat(), y.toFloat()))
-                }
-
-                points = newPoints.toList()
+                points = calculateGraphPoints(function, canvasSize, xWidth)
                 invalidations++ // Forces a redraw (recomposition) of the canvas
             } catch (_: Exception) {
                 points = emptyList()
@@ -81,6 +60,7 @@ fun AppScreen(modifier: Modifier = Modifier) {
         Column {
             Canvas(
                 Modifier
+                    .clipToBounds()
                     .weight(1f)
                     .fillMaxWidth()
             ) {
@@ -103,46 +83,73 @@ fun AppScreen(modifier: Modifier = Modifier) {
                     strokeWidth = 2.dp.toPx()
                 )
 
-                // Draw gridlines
-                for (i in 0..xWidth) {
-                    val x = (i.toDouble() - xWidth / 2) * (size.width / xWidth) + size.width / 2
-                    drawLine(
-                        color = colorScheme.surfaceDim,
-                        start = Offset(x.toFloat(), 0f),
-                        end = Offset(x.toFloat(), size.height)
-                    )
-                }
-
-                for (i in 0..(xWidth * (canvasSize.height / canvasSize.width)).toInt()) {
-                    val y = (i.toDouble() - xWidth * (canvasSize.height / canvasSize.width) / 2) * (size.height / (xWidth * (canvasSize.height / canvasSize.width))) + size.height / 2
-                    drawLine(
-                        color = colorScheme.surfaceDim,
-                        start = Offset(0f, y.toFloat()),
-                        end = Offset(size.width, y.toFloat())
-                    )
-                }
-
                 // Draw the graph
-
                 drawPoints(
                     points,
                     pointMode = PointMode.Polygon,
                     color = colorScheme.primary,
-                    strokeWidth = 4.dp.toPx()
+                    strokeWidth = 3.dp.toPx()
                 )
             }
-            Column(Modifier.padding(32.dp)) {
+            Column(
+                Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.End
+            ) {
                 OutlinedTextField(
                     value = function,
                     onValueChange = { function = it }
                 )
                 Spacer(Modifier.height(8.dp))
-                Button(onClick = { redrawCount++ }, shapes = ButtonDefaults.shapes()) {
-                    Text("Redraw")
-                }
+//                Button(onClick = { redrawCount++ }, shapes = ButtonDefaults.shapes()) {
+//                    Text("Redraw")
+//                }
             }
         }
     }
+}
+
+fun calculateGraphPoints(
+    function: String,
+    canvasSize: Size,
+    xWidth: Int
+): List<Offset> {
+    val exp = ExpressionBuilder(function)
+        .variable("x")
+        .build()
+
+    val res = exp.validate(false)
+
+    if (!res.isValid) {
+        Log.e("Graph", "Invalid function: $function")
+        throw IllegalArgumentException()
+    }
+
+    val newPoints = mutableListOf<Offset>()
+    val widthInt = canvasSize.width.toInt()
+    val yScaleFactor =
+        (canvasSize.height / (xWidth * (canvasSize.height / canvasSize.width)))
+
+    for (i in 0..widthInt) {
+        try {
+            // Calculate the value of x and y for the given i-pixel-offset
+            // x can be simply calculated by subtracting half of canvas width (to shift origin to half of the screen) and then scaling according to xWidth
+            val x = ((i.toDouble() - canvasSize.width / 2) / canvasSize.width) * xWidth
+
+            // Calculating y is a bit more complex, we find the value of f(x) for x as calculated above,
+            // then multiply it by the below factor to scale it to keep the x and y-axis scale uniform in the graph
+            // We then subtract this value from half of the canvas width (to shift origin to half the screen)
+            // And voila, we get a graph with the origin at the center of the screen.
+            val y = (-exp.setVariable("x", x).evaluate() * yScaleFactor) +
+                    canvasSize.height / 2
+            newPoints.add(Offset(i.toFloat(), y.toFloat()))
+        } catch (_: Exception) {
+            Log.e("Graph", "Cannot evaluate point:\nx = $x\nfunction = $function")
+        }
+    }
+
+    return newPoints.toList()
 }
 
 @Preview
